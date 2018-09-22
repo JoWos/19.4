35de233057ebe256e2afc1a38415bd803bd49b18
Joanna Woszczyk <woszczyk.joanna@gmail.com>
2018-09-22 22:37:56 +0200

    Add js

--- script-compiled.js
+++ script-compiled.js
@@ -1,6 +1,36 @@
 'use strict';
 
-var sayHello = function sayHello() {
-  return alert('Hello world!');
+//1
+var firstWord = 'Hello';
+var secondWord = 'World';
+var text = firstWord + ', ' + secondWord;
+
+//2
+var multiply = function multiply(a) {
+  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
+  return console.log(a * b);
 };
-sayHello();
+
+//3
+var average = function average() {
+  for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+    args[_key] = arguments[_key];
+  }
+
+  var sum = 0;
+  args.forEach(function (arg) {
+    return sum += arg / args.length;
+  });
+  return console.log(sum);
+};
+
+//4
+var grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+average.apply(undefined, grades);
+
+//5
+var strange = [1, 4, 'Iwona', false, 'Nowak'];
+var firstname = strange[2],
+    lastname = strange[4];
+
+console.log(firstname + ' ' + lastname);
--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-const sayHello = () => alert('Hello world!');
-sayHello();
+//1
+const firstWord = 'Hello';
+const secondWord ='World';
+const text = `${firstWord}, ${secondWord}`;
+
+//2
+const multiply = (a, b = 1) => console.log(a * b);
+
+//3
+const average = (...args) => {
+  let sum = 0;
+  args.forEach(arg => sum += arg/args.length);
+  return console.log(sum);
+}
+
+//4
+const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+average(...grades);
+
+//5
+const strange = [1, 4, 'Iwona', false, 'Nowak'];
+const [ , , firstname, , lastname] = strange
+console.log(`${firstname} ${lastname}`);
